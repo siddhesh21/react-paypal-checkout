d1e949a87615600ab22a69c3e434b843ae134790
Siddhesh Kankekar <kankekas@tcd.ie>
2021-03-20 09:25:47 +0000

    Added CDN for PayPal+ completed short app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import "./App.css";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
 
 function App() {
-  return <div className="App"></div>;
+  const [price, setPrice] = useState(0);
+  const createOrder = (data, actions) => {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: price, //To Charge Customers
+          },
+        },
+      ],
+    });
+  };
+
+  const onApprove = (data, actions) => {
+    console.log(data);
+    return actions.order.capture();
+  };
+
+  return (
+    <div className="app">
+      <div className="wrapper">
+        <input
+          className="app__input"
+          type="number"
+          onChange={(e) => setPrice(e.target.value)}
+          value={price}
+        />
+        <PayPalButton
+          createOrder={(data, actions) => createOrder(data, actions)}
+          onApprove={(data, actions) => onApprove(data, actions)}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
